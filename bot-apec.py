2d01d29a25705978afa80937dd45e79897bb0168
BELIARD Aurelien <aurelien.beliard@exail.com>
2023-07-12 17:54:04 +0200

    apec bot

--- bot-apec.py
+++ bot-apec.py
@@ -14,8 +14,18 @@ driver.get("https://www.apec.fr/candidat.html") #open apec website
 time.sleep(5) #wait 5 seconds       
 driver.find_element(By.NAME,"keywords").send_keys("devops") #send keywords  
 time.sleep(5) #wait 5 seconds   
-driver.find_element(By.NAME,"locationOffres").send_keys("devops") #click login button      
+where_input = driver.find_element(By.NAME,"locationOffresDisplay") #send where
+where_input.send_keys("nantes") #click login button     
+where_input.send_keys(Keys.RETURN)  
+#click button onetrust-reject-all-handler if exist
+try:    
+    driver.find_element(By.ID,"onetrust-accept-btn-handler").click()
+except:
+    pass
 
-driver.find_element_by_id("username").send_keys("username") #enter username
-driver.find_element_by_id("password").send_keys("password") #enter password
+#find button type="submit"
+# button = driver.find_element(By.CSS_SELECTOR,"button[type='submit']")            
+# button.click()
+# driver.find_element_by_id("username").send_keys("username") #enter username
+# driver.find_element_by_id("password").send_keys("password") #enter password
 time.sleep(5) #wait 5 seconds   
