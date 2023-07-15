018065312b03b659b5ff3d3761a83e8e3640a317
aurelBZH <aurelien.beliard@laposte.net>
2023-07-15 14:57:27 +0200

    var not defined

--- highlight.py
+++ highlight.py
@@ -1,10 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import ScriptTimeoutException
+
 def highlight_element(element):
     driver = element._parent
 
